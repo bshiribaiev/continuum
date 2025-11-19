a6959b25e490ccd137e03ea75bc6e2bbdeb931d3
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-11-18 22:46:26 -0500

    update backend setup

--- backend/src/main/java/com/continuum/App.java
+++ backend/src/main/java/com/continuum/App.java
@@ -1,13 +1,13 @@
-package com.continuum.backend;
+package com.continuum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
--- backend/src/test/java/com/continuum/AppTests.java
+++ backend/src/test/java/com/continuum/AppTests.java
@@ -1,10 +1,10 @@
-package com.continuum.backend;
+package com.continuum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class AppTests {
 
 	@Test
 	void contextLoads() {
