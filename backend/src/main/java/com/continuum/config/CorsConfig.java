2cdeaf011a33beca41802662be0f2a0ce602c95a
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-12-06 15:24:09 -0500

    update CORS and appilcation props

--- backend/src/main/java/com/continuum/config/CorsConfig.java
+++ backend/src/main/java/com/continuum/config/CorsConfig.java
@@ -1,11 +1,11 @@
-package com.continuum;
+package com.continuum.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
